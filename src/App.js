6a3f39b053c2dfc3ca18c9f540ea75ce1d6162b0
Nicky <nickshah534@gmail.com>
2023-10-10 10:43:10 +1100

    multipage

--- src/App.js
+++ src/App.js
@@ -8,24 +8,34 @@ import { Info } from './Components/Info';
 import { Navbar } from './Components/Navbar';
 import { Projects } from './Components/Projects';
 import { Skills } from './Components/Skills';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <><input type="checkbox" id="darkMode"/>
     <div className="wrapper">
     <label for="darkMode" class="dark-mode-toggle"><i class="fa-solid fa-circle-half-stroke"></i></label>
-
-     <Navbar />
-     <Hero />
-     < Info />
+    <Navbar />
+    <Routes>
+      <Route path="/" element={ <Hero />}/>
+      <Route path="/" element={ < Info />}/>
+      <Route path="/" element={<Skills />}/>
+      <Route path="/" element={< Projects />}/>
+      <Route path="/" element={ <About />}/>
+      <Route path="/" element={ <Contact/>}/>
+      <Route path="/" element={ <Footer /> }/>
+    </Routes>
+     
+    
+     {/* < Info />
      <Skills />
      < Projects />
      <About />
      <Contact/>
-     <Footer />
+     <Footer /> */}
     </div>
     </>
   );
-}
+};
 
 export default App;
