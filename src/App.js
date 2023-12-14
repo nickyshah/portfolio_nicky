d4cbd22947d446e703fb4a9e408dd216644fde47
Nicky <nickshah534@gmail.com>
2023-12-14 20:19:13 +1100

    animation added to the hmburger menu

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Info } from './Components/Info';
 import { Navbar } from './Components/Navbar';
 import { Projects } from './Components/Projects';
 import { Skills } from './Components/Skills';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
   return (
